929db220e6d72a2a376266f64945fefcb98839bc
User0033 <nikola_g43@yahoo.com~>
2026-05-02 20:04:36 +0200

    output option

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  output: "export",
+  output: "standalone",
   distDir: "dist",
   env: {
     HOSTINGER_EMAIL: process.env.HOSTINGER_EMAIL,
