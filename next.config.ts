ecabf8149b72c89e4342535ec8eff64c153d80f2
User0033 <nikola_g43@yahoo.com~>
2026-07-04 14:03:34 +0200

    Update nextConfig

--- next.config.ts
+++ next.config.ts
@@ -2,12 +2,6 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: "export",
-  distDir: "dist",
-  images: { unoptimized: true },
-  env: {
-    HOSTINGER_EMAIL: process.env.HOSTINGER_EMAIL,
-    HOSTINGER_PASSWORD: process.env.HOSTINGER_PASSWORD,
-  },
 };
 
 export default nextConfig;
